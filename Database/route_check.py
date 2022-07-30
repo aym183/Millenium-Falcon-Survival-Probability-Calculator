3e2d645a2b7d4131cf0c3093288288ddb6f2fc24
Ayman Ali <mujayman@amazon.com>
2022-07-30 12:44:06 +0100

    Added path cost

--- Database/route_check.py
+++ Database/route_check.py
@@ -6,12 +6,44 @@ class Routes():
         self.departure = departure
         self.destination = destination
         self.current_location = departure
+        self.next_location = None
+        self.path=[]
+        self.cost = 0
+        self.cost_list = []
+        
+
+    visited = set() # Set to keep track of visited nodes of graph.
+
+    def dfs(self, visited, node):  #function for dfs 
+
+        c = conn.cursor()
+        c.execute(f"SELECT * FROM ROUTES WHERE ORIGIN == '{self.current_location}'")
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
 
     def check_routes(self):
+    
         c = conn.cursor()
-        c.execute(f"SELECT * FROM ROUTES WHERE ORIGIN == '{self.departure}'")
+        c.execute(f"SELECT * FROM ROUTES WHERE ORIGIN == '{self.current_location}'")
        
         for i in c.fetchall():
-            print("I am here")
-            print(i)
+            if i[1] == self.destination:
+                print(i[1])
+                print('FINISH')
+                self.cost += i[2]
+                self.cost_list.append(self.cost)
+                print(self.cost)
+                self.cost = 0
+
+                
+            else:
+                self.path.append(i[1])
+                print(i[1])
+                self.current_location = i[1]
+                self.cost += i[2]
+                self.check_routes()
+                
+    
         conn.commit()
+    
